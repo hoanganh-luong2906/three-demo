505db03f2c0893eda23a8ee1e84b36151ecd73ce
Hoang Anh <hoanganh.dev.it@gmail.com>
2025-04-13 08:09:32 +0700

    feat: Add more feature

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,19 @@ import Plane from './components/Plane';
 import Sphere from './components/Sphere';
 
 function App() {
+  const onCreated = ({ gl }: any) => {
+    gl.setClearColor(new THREE.Color('#E9E9E9'), 1); //method 1
+  };
+
   return (
     <div className="h-screen w-screen">
       <StrictMode>
-        <Canvas dpr={[1, 2]} gl={{ antialias: true, toneMapping: THREE.CineonToneMapping }}>
+        <Canvas dpr={[1, 2]} gl={{ antialias: true, toneMapping: THREE.CineonToneMapping }} onCreated={onCreated}>
           <OrbitControls makeDefault />
-
+          <color args={['#E9E9E9']} attach={'background'} /> //method 2
           <ambientLight intensity={0.5} />
           <pointLight position={[10, 10, 10]} />
           <pointLight position={[-10, -10, -10]} />
-
           {/* <Text /> */}
           <Sphere />
           <Plane />
